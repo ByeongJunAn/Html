a9c4932726166f9e172d5bfd3eefd0b9c31ea01f
ByeongJunAn <abj5237@nate.com>
2022-11-28 04:03:44 +0900

    home

--- 네카라쿠배_2조/js/ahn.js
+++ 네카라쿠배_2조/js/ahn.js
@@ -1,22 +1,83 @@
+//스크롤 변경시 배너 투명화
+function scrollMove() {
+    $(function () {
+        $(window).scroll(function () {
+            if ($(document).scrollTop() > 100) {
+                $("#fix_header_nav").css('background-color', '#C8DBBE ');
+            } else {
+                $("#fix_header_nav").css('background-color', 'transparent ');
+            };
+        });
+    });
+};
 
+//갤러리 둘러보기 바꾸기
+function GBI() {
+    setTimeout(change_img1, 2000);
+};
 
-var slideIndex = 0;
-showSlides();
+function change_img1() {
+    $("#div_half_division2").css({ "background": "url(../image/BI_sea_dog.jpg)" });
+    $("#div_half_division2").css({ "background-size": "100% 100%" });
+    //$('.linkButton').prop('href', 변경할 값) '.linkButton' 이거 만들어서 href="" 테그 변경가능
+    setTimeout(change_img2, 2000);
+};
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); // 3초마다 바뀜
+function change_img2() {
+    $("#div_half_division2").css({ "background": "url(../image/BI_happy_dog.jpg)" });
+    $("#div_half_division2").css({ "background-size": "100% 100%" });
+    //$('.linkButton').prop('href', 변경할 값)
+    setTimeout(change_img3, 2000);
+};
+
+function change_img3() {
+    $("#div_half_division2").css({ "background": "url(../image/BI_sleep_dog.jpg)" });
+    $("#div_half_division2").css({ "background-size": "100% 100%" });
+    //$('.linkButton').prop('href', 변경할 값)
+    setTimeout(change_img1, 2000);
+};
+
+// 숫자들 변경
+
+function p_text_change_in(n, msg1, msg2) {
+    $("#div_quadrant" + n).mouseover(() => {
+        $("#p_v_text" + n).text(msg1);
+        $("#div_quadrant" + n).addClass("div_change");
+        $("#p_v_text" + n).addClass("p_change");
+        for (let i = 1; i <= 5; i++) {
+            if (i == n) continue;
+            $("#div_quadrant" + i).addClass("div_hidden_area");
+        }
+    });
+    $("#div_quadrant" + n).mouseout(() => {
+        $("#p_v_text" + n).text(msg2);
+        $("#div_quadrant" + n).removeClass("div_change");
+        $("#p_v_text" + n).removeClass("p_change");
+        for (let i = 1; i <= 5; i++) {
+            if (i == n) continue;
+            $("#div_quadrant" + i).removeClass("div_hidden_area");
+        }
+    });
+};
+
+// 기업가치 가기
+function div_left_move() {
+    $(".div_Lslide").addClass("div_Lslide_move");
+    $("#p_comper_velue").css('display', 'none');
+    setTimeout(visible_p, 900);
+}
+function visible_p() {
+    $(".p_check_v").css('display', 'block');
 }
 
+
+
+/*------------------------------------------*/
+/*이미지 모달*/
+function myFunction(imgs) {
+    var tapexpandImg = document.getElementById("tapexpandedImg");
+    var tapimgText = document.getElementById("tapimgtext");
+    tapexpandImg.src = imgs.src;
+    tapimgText.innerHTML = imgs.alt;
+    tapexpandImg.parentElement.style.display = "block";
+  }
--- 네카라쿠배_2조/js/ahn_f1.js
+++
@@ -1,7 +0,0 @@
-
- var w;
- function openWindow(){
-     w = window.open(' ', ' ', 'width=1200, height=1100');
-     w.document.write("<p>오늘 다음과 같은 상품을 싸게 팝니다.</p>");
-     w.moveTo(0,0);
- }
